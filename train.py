558fbe0cfcfdd3509915a0429c011221807cd890
martinwilk <75608604+martinwilk@users.noreply.github.com>
2020-12-07 10:36:15 +0000

    reorganized train.py and added data import

--- train.py
+++ train.py
@@ -10,17 +10,8 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
 
 run = Run.get_context()
 
@@ -49,7 +40,8 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+    return x_df, y_df
+
 
 def main():
     # Add arguments to script
@@ -69,4 +61,10 @@ def main():
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
+    url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    ds = TabularDatasetFactory.from_delimited_files(url)
+    x, y = clean_data(ds)
+    x_train, y_train, x_test, y_test = train_test_split(x, y, train_size=0.7, random_state=42, stratify=y) 
     main()
+
+
